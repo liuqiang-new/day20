c916d6295991796c40f7b9457965304503a8382c
liuqiang <416804827@qq.com>
2021-05-24 14:54:22 +0800

    新项目：学生管理系统

---
+++ src/com/offcn/entity/Student.java
@@ -0,0 +1,37 @@
+package com.offcn.entity;
+
+public class Student {
+    private int sid;
+    private String sname;
+    private int sage;
+
+    public Student(int sid, String sname, int sage) {
+        this.sid = sid;
+        this.sname = sname;
+        this.sage = sage;
+    }
+
+    public int getSid() {
+        return sid;
+    }
+
+    public void setSid(int sid) {
+        this.sid = sid;
+    }
+
+    public String getSname() {
+        return sname;
+    }
+
+    public void setSname(String sname) {
+        this.sname = sname;
+    }
+
+    public int getSage() {
+        return sage;
+    }
+
+    public void setSage(int sage) {
+        this.sage = sage;
+    }
+}
